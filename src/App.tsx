6745e97c417ae93693348e14ea49dadbfa1c50a5
rifatsaown <rifatsaown0@gmail.com>
2022-10-13 20:09:40 +0600

    api loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-        <h1>Hello CodeSandbox</h1>
+        <LoadCountries></LoadCountries>
     </div>
   )
 }
+
+const LoadCountries = () => {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+      fetch("https://restcountries.com/v3.1/all")
+        .then((res) => res.json())
+        .then((data) => setCountries(data));
+    }, []);
+    return (
+        <div className="App">
+            <p>Countries {countries.length}</p>
+        </div>
+    )
+};
+
